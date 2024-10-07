ab2398dc5effaa2cdf5587b7561cc56af4cadb4c
Kunal Bhoir <125652272+Kunalbhoir02@users.noreply.github.com>
2024-10-07 15:43:57 +0530

    snake game update

---
+++ model rendering5/model rendering5/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace model_rendering5
+{
+    public partial class Form1 : Form
+    {
+        private Microsoft.DirectX.Direct3D.Device device;
+        private Microsoft.DirectX.Direct3D.Texture texture;
+        private Microsoft.DirectX.Direct3D.Font font;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            this.Load += new EventHandler(Form1_Load); // Hook up the Load event
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeDirectX();
+        }
+
+        private void InitializeDirectX()
+        {
+            InitDevice();
+            InitFont();
+            LoadTexture();
+        }
+
+        private void InitFont()
+        {
+            System.Drawing.Font f = new System.Drawing.Font("Arial", 16f, FontStyle.Regular);
+            font = new Microsoft.DirectX.Direct3D.Font(device, f);
+        }
+
+        private void LoadTexture()
+        {
+            try
+            {
+                texture = TextureLoader.FromFile(device, "E:\\TYCS\\images\\img1.jpg", 400, 400, 1, 0, Format.A8B8G8R8, Pool.Managed, Filter.Point, Filter.Point, Color.Transparent.ToArgb());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading texture: " + ex.Message);
+            }
+        }
+
+        private void InitDevice()
+        {
+            PresentParameters pp = new PresentParameters();
+            pp.Windowed = true;
+            pp.SwapEffect = SwapEffect.Discard;
+            device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, pp);
+        }
+
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.CornflowerBlue, 0, 1);
+            device.BeginScene();
+            using (Sprite s = new Sprite(device))
+            {
+                s.Begin(SpriteFlags.AlphaBlend);
+                s.Draw2D(texture, new Rectangle(0, 0, 0, 0), new Rectangle(0, 0, device.Viewport.Width, device.Viewport.Height), new Point(0, 0), 0f, new Point(0, 0), Color.White);
+                font.DrawText(s, "KMA College", new Point(10, 10), Color.Black);
+                s.End();
+            }
+            device.EndScene();
+            device.Present();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (texture != null) texture.Dispose();
+            if (font != null) font.Dispose();
+            if (device != null) device.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
--- snake game.py
+++ snake game.py
@@ -7,63 +7,65 @@ pygame.init()
 
 # Colors
 white = (255, 255, 255)
+yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 
-# Display settings
+# Window dimensions
 width = 600
 height = 400
-display = pygame.display.set_mode((width, height))
+
+# Create the game window
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Snake Game')
 
-# Game settings
+# Clock to control game speed
 clock = pygame.time.Clock()
+
+# Snake settings
 snake_block = 10
 snake_speed = 15
 
-# Font style
+# Font styles
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
-def score(score):
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, black, [x[0], x[1], snake_block, snake_block])
+
+def your_score(score):
     value = score_font.render("Score: " + str(score), True, black)
-    display.blit(value, [10, 10])
+    screen.blit(value, [0, 0])
 
-def draw_snake(snake_block, snake_list):
-    for segment in snake_list:
-        pygame.draw.rect(display, black, [segment[0], segment[1], snake_block, snake_block])
-
-def display_message(msg, color):
+def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    display.blit(mesg, [width / 6, height / 3])
+    screen.blit(mesg, [width / 6, height / 3])
 
-def game_loop():
+def gameLoop():  # Main function
     game_over = False
     game_close = False
 
-    # Starting position of the snake
     x1 = width / 2
     y1 = height / 2
+
     x1_change = 0
     y1_change = 0
 
     snake_list = []
     length_of_snake = 1
 
-    # Random position for food
     foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
 
-    current_score = 0
-
     while not game_over:
 
-        while game_close:
-            display.fill(blue)
-            display_message("You Lost! Press C-Play Again or Q-Quit", red)
-            score(current_score)
+        while game_close == True:
+            screen.fill(blue)
+            message("You Lost! Press C-Play Again or Q-Quit", red)
+            your_score(length_of_snake - 1)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -72,7 +74,7 @@ def game_loop():
                         game_over = True
                         game_close = False
                     if event.key == pygame.K_c:
-                        game_loop()
+                        gameLoop()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -91,14 +93,13 @@ def game_loop():
                     y1_change = snake_block
                     x1_change = 0
 
-        # Check for collision with boundaries
         if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
             game_close = True
 
         x1 += x1_change
         y1 += y1_change
-        display.fill(blue)
-        pygame.draw.rect(display, green, [foodx, foody, snake_block, snake_block])
+        screen.fill(blue)
+        pygame.draw.rect(screen, green, [foodx, foody, snake_block, snake_block])
         snake_head = []
         snake_head.append(x1)
         snake_head.append(y1)
@@ -106,27 +107,23 @@ def game_loop():
         if len(snake_list) > length_of_snake:
             del snake_list[0]
 
-        # Check for collision with itself
-        for segment in snake_list[:-1]:
-            if segment == snake_head:
+        for x in snake_list[:-1]:
+            if x == snake_head:
                 game_close = True
 
-        draw_snake(snake_block, snake_list)
-        score(current_score)
+        our_snake(snake_block, snake_list)
+        your_score(length_of_snake - 1)
 
         pygame.display.update()
 
-        # Check if the snake eats the food
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
             length_of_snake += 1
-            current_score += 1
 
         clock.tick(snake_speed)
 
     pygame.quit()
     quit()
 
-# Start the game
-game_loop()
+gameLoop()
